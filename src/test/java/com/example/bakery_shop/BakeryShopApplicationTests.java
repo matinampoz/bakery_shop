661e7b922b4cb994cc65c99b600faa516edda451
matinampoz <matinampoz@gmail.com>
2024-05-14 11:24:15 +0300

    Initial commit

---
+++ src/test/java/com/example/bakery_shop/BakeryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bakery_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
